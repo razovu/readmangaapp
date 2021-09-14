bfd0dca97511fdf6145848686793c3acd63a7dac
nailurazov <razovu@gmail.com>
2021-09-14 14:20:10 +0300

    add screen classes and dependencies

---
+++ app/src/main/java/com/example/readmangaapp/screens/ProfileFragment.kt
@@ -0,0 +1,12 @@
+package com.example.readmangaapp.screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.readmangaapp.R
+
+class ProfileFragment : Fragment() {
+
+}
