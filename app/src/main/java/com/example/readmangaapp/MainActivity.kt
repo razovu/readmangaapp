abedda580559029f878b6b753cc7a74094ca54d7
nailurazov <razovu@gmail.com>
2021-09-14 14:11:05 +0300

    Initial commit

---
+++ app/src/main/java/com/example/readmangaapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.readmangaapp
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.readmangaapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
